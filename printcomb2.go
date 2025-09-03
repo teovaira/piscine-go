b7dbe1583a4c3aaf6aecf157521ce32e2fbac887
tvairakt <t.vairaktaris@gmail.com>
2025-09-03 20:19:32 +0300

    fix: show the pairs in ascending order

--- printcomb2.go
+++ printcomb2.go
@@ -3,16 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for r := '0'; r <= '9'; r++ {
-		for j := '0'; j <= '9'; j++ {
+	for r := 0; r <= 98; r++ {
+		for j := 0; j <= 99; j++ {
 			if r != j {
-				z01.PrintRune(rune(0 + '0'))
-				z01.PrintRune(rune(0 + '0'))
+				z01.PrintRune(rune('0' + r/10))
+				z01.PrintRune(rune(r%10 + '0'))
 				z01.PrintRune(' ')
-				z01.PrintRune(r)
-				z01.PrintRune(j)
+				z01.PrintRune(rune(j/10 + '0'))
+				z01.PrintRune(rune(j%10 + '0'))
 
-				if !(r == '9' && j == '8') {
+				if !(r == 98 && j == 99) {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
