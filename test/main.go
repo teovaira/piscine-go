08179c00b89067fef1e8db91d974c8f32378d77b
tvairakt <t.vairaktaris@gmail.com>
2025-09-04 14:52:43 +0300

    dive deeper in pointers

--- test/main.go
+++ test/main.go
@@ -2,13 +2,17 @@ package main
 
 // import (
 // 	"fmt"
+
 // 	"piscine"
 // )
 
 // func main() {
-// 	a := 0
-// 	b := &a
-// 	n := &b
-// 	piscine.UltimatePointOne(&n)
-// 	fmt.Println(a)
+// 	a := 13
+// 	b := 2
+
+// 	var div int
+// 	var mod int
+// 	piscine.DivMod(a, b, &div, &mod)
+// 	fmt.Println(div)
+// 	fmt.Println((mod))
 // }
