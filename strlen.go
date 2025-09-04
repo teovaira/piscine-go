861ad7414571954748948bf828a75fa3a84e3b69
tvairakt <t.vairaktaris@gmail.com>
2025-09-04 16:22:49 +0300

    fix: solve with count variable according to instructions

--- strlen.go
+++ strlen.go
@@ -1,17 +1,15 @@
 package piscine
 
-import "unicode/utf8"
-
 func StrLen(s string) int {
-	// count := 0
-	// for range s {
-	// 	count++
-	// }
+	count := 0
+	for range s {
+		count++
+	}
 
-	// return count
+	return count
 
 	// rs := []rune(s)
 	// return len(rs)
 
-	return utf8.RuneCountInString(s)
+	// return utf8.RuneCountInString(s)
 }
