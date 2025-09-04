0ac5fdcc7e5e401c6cf63305b117795e8f987fc7
tvairakt <t.vairaktaris@gmail.com>
2025-09-04 21:41:32 +0300

    fix

--- test/main.go
+++ test/main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"piscine"
-)
+// 	"piscine"
+// )
 
-func main() {
-	fmt.Println(piscine.Atoi("12345"))
-	fmt.Println(piscine.Atoi("0000000012345"))
-	fmt.Println(piscine.Atoi("012 345"))
-	fmt.Println(piscine.Atoi("Hello World!"))
-	fmt.Println(piscine.Atoi("+1234"))
-	fmt.Println(piscine.Atoi("-1234"))
-	fmt.Println(piscine.Atoi("++1234"))
-	fmt.Println(piscine.Atoi("--1234"))
-}
+// func main() {
+// 	fmt.Println(piscine.Atoi("12345"))
+// 	fmt.Println(piscine.Atoi("0000000012345"))
+// 	fmt.Println(piscine.Atoi("012 345"))
+// 	fmt.Println(piscine.Atoi("Hello World!"))
+// 	fmt.Println(piscine.Atoi("+1234"))
+// 	fmt.Println(piscine.Atoi("-1234"))
+// 	fmt.Println(piscine.Atoi("++1234"))
+// 	fmt.Println(piscine.Atoi("--1234"))
+// }
