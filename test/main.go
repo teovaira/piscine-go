091b708b82855f6bc097afa82bc6946b87844162
tvairakt <t.vairaktaris@gmail.com>
2025-09-06 18:19:59 +0300

    create the quadb function for quad project

--- test/main.go
+++ test/main.go
@@ -1,13 +1,14 @@
 package main
 
-// import (
-// 	"fmt"
+import "piscine"
 
-// 	"piscine"
-// )
 
-// func main() {
-// 	s := []int{5, 4, 3, 2, 1, 0}
-// 	piscine.SortIntegerTable(s)
-// 	fmt.Println(s)
-// }
+func main() {
+
+	piscine.QuadB(6, 6)	
+	piscine.QuadB(5,1)
+	piscine.QuadB(5,3)
+	piscine.QuadB(1,1)
+	piscine.QuadB(1,5)
+
+}
