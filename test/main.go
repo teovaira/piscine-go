0ea11017514c80d1cd8dd1fbff89d1f3d0c0ce93
tvairakt <t.vairaktaris@gmail.com>
2025-09-06 19:25:28 +0300

    create quadC function for quad project

--- test/main.go
+++ test/main.go
@@ -5,10 +5,10 @@ import "piscine"
 
 func main() {
 
-	piscine.QuadB(6, 6)	
-	piscine.QuadB(5,1)
-	piscine.QuadB(5,3)
-	piscine.QuadB(1,1)
-	piscine.QuadB(1,5)
+
+	piscine.QuadC(5,1)
+	piscine.QuadC(5,3)
+	piscine.QuadC(1,1)
+	piscine.QuadC(1,5)
 
 }
