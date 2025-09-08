b14197165c32ed30d60b812b887b8e897252eda9
tvairakt <t.vairaktaris@gmail.com>
2025-09-08 11:01:46 +0300

    fix: comment out quad functions

--- quadB.go
+++ quadB.go
@@ -1,48 +1,47 @@
 package piscine
 
-import "fmt"
+// import "fmt"
 
-func QuadB(x, y int) {
+// func QuadB(x, y int) {
 
-	if x <= 0 || y <= 0 {
-		return
-	}
+// 	if x <= 0 || y <= 0 {
+// 		return
+// 	}
 
-	for i := 0; i < x; i++ {
-		if i == 0 {
-			fmt.Print("/")
-		} else if i == x-1 {
-			fmt.Print("\\")
-		} else {
-			fmt.Print("*")
-		}
-	}
+// 	for i := 0; i < x; i++ {
+// 		if i == 0 {
+// 			fmt.Print("/")
+// 		} else if i == x-1 {
+// 			fmt.Print("\\")
+// 		} else {
+// 			fmt.Print("*")
+// 		}
+// 	}
 
-	fmt.Println()
+// 	fmt.Println()
 
-	for j := 0; j < y-2; j++ {
-		for i := 0; i < x; i++ {
-			if i == 0 || i == x-1 {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		
-		fmt.Println()
-	}
+// 	for j := 0; j < y-2; j++ {
+// 		for i := 0; i < x; i++ {
+// 			if i == 0 || i == x-1 {
+// 				fmt.Print("*")
+// 			} else {
+// 				fmt.Print(" ")
+// 			}
+// 		}
 
-	
-	if y >= 2 {
-		for i := 0; i < x; i++ {
-			if i == 0 {
-				fmt.Print("\\")
-			} else if i == x-1 {
-				fmt.Print("/")
-			} else {
-				fmt.Print("*")
-			}
-		}
-		fmt.Println()
-	}
-}
+// 		fmt.Println()
+// 	}
+
+// 	if y >= 2 {
+// 		for i := 0; i < x; i++ {
+// 			if i == 0 {
+// 				fmt.Print("\\")
+// 			} else if i == x-1 {
+// 				fmt.Print("/")
+// 			} else {
+// 				fmt.Print("*")
+// 			}
+// 		}
+// 		fmt.Println()
+// 	}
+// }
--- quadC.go
+++ quadC.go
@@ -1,49 +1,49 @@
 package piscine
 
-import "fmt"
+// import "fmt"
 
-func QuadC(x, y int) {
-	if x <= 0 || y <= 0 {
-		return
-	}
+// func QuadC(x, y int) {
+// 	if x <= 0 || y <= 0 {
+// 		return
+// 	}
 
-	for i := 1; i <= y; i++ {
+// 	for i := 1; i <= y; i++ {
 
-		for j := 1; j <= x; j++ {
+// 		for j := 1; j <= x; j++ {
 
-			var s string
+// 			var s string
 
-			isTop := i == 1
-			isBottom := i == y
-			isLeft := j == 1
-			isRight := j == x
+// 			isTop := i == 1
+// 			isBottom := i == y
+// 			isLeft := j == 1
+// 			isRight := j == x
 
-			switch {
+// 			switch {
 
-			case isTop && isLeft:
-				s = "A"
-			case isTop && isRight:
-				s = "A"
-			case isTop:
-				s = "B"
+// 			case isTop && isLeft:
+// 				s = "A"
+// 			case isTop && isRight:
+// 				s = "A"
+// 			case isTop:
+// 				s = "B"
 
-			case isBottom && isLeft:
-				s = "C"
-			case isBottom && isRight:
-				s = "C"
-			case isBottom:
-				s = "B"
+// 			case isBottom && isLeft:
+// 				s = "C"
+// 			case isBottom && isRight:
+// 				s = "C"
+// 			case isBottom:
+// 				s = "B"
 
-			case isLeft || isRight:
-				s = "B"
+// 			case isLeft || isRight:
+// 				s = "B"
 
-			default:
-				s = ""
-			}
-			fmt.Print(s)
-		}
+// 			default:
+// 				s = ""
+// 			}
+// 			fmt.Print(s)
+// 		}
 
-		fmt.Println()
+// 		fmt.Println()
 
-	}
-}
+// 	}
+// }
