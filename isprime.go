16bb9fd0094cb663c028427a9b7393c5d68c3d1c
tvairakt <t.vairaktaris@gmail.com>
2025-09-08 14:41:31 +0300

    refactor: change the condition in the for loop

--- isprime.go
+++ isprime.go
@@ -5,8 +5,16 @@ func IsPrime(nb int) bool {
 		return false
 	}
 
-	for i := 2; i < nb; i++ {
-		if nb%i == 0 && i*i == nb {
+	if nb == 2 {
+		return true
+	}
+
+	if nb%2 == 0 {
+		return false
+	}
+
+	for i := 3; i*i < nb; i += 2 {
+		if nb%i == 0 {
 			return false
 		}
 	}
