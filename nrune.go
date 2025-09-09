a304b4a50eb67d1815e294974492e39c483bf3ee
tvairakt <t.vairaktaris@gmail.com>
2025-09-09 12:15:34 +0300

    create a function that acts like the strings.compare

--- nrune.go
+++ nrune.go
@@ -2,10 +2,10 @@ package piscine
 
 func NRune(s string, n int) rune {
 	if n > 0 && n <= len([]rune(s)) {
-		for range s {
-			runes := []rune(s)
-			return runes[n-1]
-		}
+
+		runes := []rune(s)
+		return runes[n-1]
+
 	}
 
 	return 0
--- test/main.go
+++ test/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"piscine"
+	"fmt"
 
-	"github.com/01-edu/z01"
+	"piscine"
 )
 
 func main() {
-	z01.PrintRune(piscine.NRune("Hello!", 3))
-	z01.PrintRune(piscine.NRune("Salut!", 2))
-	z01.PrintRune(piscine.NRune("Bye!", -1))
-	z01.PrintRune(piscine.NRune("Bye!", 5))
-	z01.PrintRune(piscine.NRune("Ola!", 4))
-	z01.PrintRune('\n')
+	fmt.Println(piscine.Compare("Hello!", "Hello!"))
+	fmt.Println(piscine.Compare("Salut!", "lut!"))
+	fmt.Println(piscine.Compare("Ola!", "Ol"))
 }
