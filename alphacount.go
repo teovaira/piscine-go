4440cd2ac13935ea7c9b68d9b4a58e99b549af27
tvairakt <t.vairaktaris@gmail.com>
2025-09-09 12:34:50 +0300

    fix: alter the if condition to give the right output

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for _, r := range s {
-		if (r >= 'A' || r >= 'a') && (r <= 'Z' || r <= 'z') && r != ' ' {
+		if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') {
 			count++
 		}
 	}
