8047f0bab82b1e2235ec9667cadfefae2bb1f811
tvairakt <t.vairaktaris@gmail.com>
2025-09-09 15:21:29 +0300

    fix: comment out the test code

--- test/main.go
+++ test/main.go
@@ -1,14 +1,14 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"piscine"
-)
+// 	"piscine"
+// )
 
-func main() {
-	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
-	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
-	fmt.Println(piscine.IsAlpha("What's this 4?"))
-	fmt.Println(piscine.IsAlpha("Whatsthis4"))
-}
+// func main() {
+// 	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
+// 	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
+// 	fmt.Println(piscine.IsAlpha("What's this 4?"))
+// 	fmt.Println(piscine.IsAlpha("Whatsthis4"))
+// }
