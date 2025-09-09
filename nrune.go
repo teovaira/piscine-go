5f770313b3d4c17c7c9abe8ddaee36e063e152cb
tvairakt <t.vairaktaris@gmail.com>
2025-09-09 11:49:56 +0300

    fix: fix the bug for the n == 0 case

--- nrune.go
+++ nrune.go
@@ -1,7 +1,7 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	if n >= 0 && n <= len([]rune(s)) {
+	if n > 0 && n <= len([]rune(s)) {
 		for range s {
 			runes := []rune(s)
 			return runes[n-1]
