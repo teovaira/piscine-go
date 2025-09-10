3035ce48885a2c70069aa53c5c93295237e0d531
tvairakt <t.vairaktaris@gmail.com>
2025-09-10 17:58:23 +0300

    create the printnbrbase

---
+++ printnbrbase.go
@@ -0,0 +1,47 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func isValidBase(base string) bool {
+	if len(base) < 2 {
+		return false
+	}
+	seen := make(map[rune]bool)
+	for _, r := range base {
+		if r == '+' || r == '-' {
+			return false
+		}
+		if seen[r] {
+			return false
+		}
+		seen[r] = true
+	}
+	return true
+}
+
+func PrintNbrBase(nbr int, base string) {
+	if !isValidBase(base) {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return
+	}
+
+	baseRunes := []rune(base)
+	baseLen := int64(len(baseRunes))
+	n := int64(nbr)
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+
+	var printRec func(int64)
+	printRec = func(num int64) {
+		if num >= baseLen {
+			printRec(num / baseLen)
+		}
+		z01.PrintRune(baseRunes[num%baseLen])
+	}
+
+	printRec(n)
+}
--- test/main.go
+++ test/main.go
@@ -1,11 +1,20 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"piscine"
-// )
+import (
+	"piscine"
 
-// func main() {
-// 	toConcat := []string{"Hello!", " How", " are", " you?"}
-// 	fmt.Println(piscine.Join(toConcat, ":"))
-// }
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	piscine.PrintNbrBase(125, "0123456789")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "01")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(125, "0123456789ABCDEF")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "choumi")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(125, "aa")
+	z01.PrintRune('\n')
+}
