1295e926fdb4d60ed49da65cba9bfd0ba96f20bb
tvairakt <t.vairaktaris@gmail.com>
2025-09-10 10:47:07 +0300

    create the capitalize function

---
+++ capitalize.go
@@ -0,0 +1,25 @@
+package piscine
+
+func Capitalize(s string) string {
+	runes := []rune(s)
+	newWord := true
+
+	for i, r := range runes {
+		if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') {
+			if newWord {
+				if r >= 'a' && r <= 'z' {
+					runes[i] = r - 32 
+				}
+				newWord = false
+			} else {
+				if r >= 'A' && r <= 'Z' {
+					runes[i] = r + 32 
+				}
+			}
+		} else {
+			newWord = true
+		}
+	}
+
+	return string(runes)
+}
