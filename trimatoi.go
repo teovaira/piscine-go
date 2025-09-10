dbaf091eff5704ecabd1ff1df32d76226cb6e70a
tvairakt <t.vairaktaris@gmail.com>
2025-09-10 09:52:41 +0300

    fix: format the code to pass the tester

--- trimatoi.go
+++ trimatoi.go
@@ -1,26 +1,24 @@
 package piscine
 
 func TrimAtoi(s string) int {
-	sign := 1        
-	number := 0         
-	foundDigit := false 
+	sign := 1
+	number := 0
+	foundDigit := false
 
-	for _, r := range s {         
+	for _, r := range s {
 		if !foundDigit && r == '-' {
-			
 			sign = -1
 			continue
 		}
 
-		if r >= '0' && r <= '9' {           
+		if r >= '0' && r <= '9' {
 			foundDigit = true
-			number = number*10 + int(r-'0')        
+			number = number*10 + int(r-'0')
 		}
-		
 	}
 
 	if !foundDigit {
-		return 0 
+		return 0
 	}
 	return sign * number
 }
