d515671a75aa178f0aa725cb9c4d99b4248af084
tvairakt <t.vairaktaris@gmail.com>
2025-09-11 16:18:55 +0300

    create a program that takes a valid integer and match it to the corresponding latin letter and if there is a --upper flag at the beginning, it capitalizes the letters

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	
+	args := os.Args[1:]
+
+	
+	upper := false
+	start := 0
+	if len(args) > 0 && args[0] == "--upper" {
+		upper = true
+		start = 1 
+	}
+
+	for i := start; i < len(args); i++ {
+		n, ok := atoiStrict(args[i]) 
+		if !ok || n < 1 || n > 26 {
+			z01.PrintRune(' ') 
+			continue
+		}
+
+		var r rune
+		if upper {
+			r = rune('A' + (n - 1)) 
+		} else {
+			r = rune('a' + (n - 1)) 
+		}
+		z01.PrintRune(r)
+	}
+
+	z01.PrintRune('\n')
+}
+
+
+func atoiStrict(s string) (int, bool) {
+	if len(s) == 0 {
+		return 0, false
+	}
+	val := 0
+	for _, r := range s { 
+		if r < '0' || r > '9' {
+			return 0, false
+		}
+		d := int(r - '0')   
+		val = val*10 + d    
+	}
+	return val, true
+}
