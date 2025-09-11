cdbdf715b1467695b635c548e17109e5772e7bc9
tvairakt <t.vairaktaris@gmail.com>
2025-09-11 11:05:10 +0300

    create a program tha prints the arguments passed in the command line

---
+++ printparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	for _, arg := range args {
+
+		for _, r := range arg {
+			z01.PrintRune(r)
+		}
+
+		z01.PrintRune('\n')
+	}
+}
