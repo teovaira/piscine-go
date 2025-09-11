e46bb2b116ac437976ecfdde2f7e295642c01693
tvairakt <t.vairaktaris@gmail.com>
2025-09-11 18:24:04 +0300

    fix: format fix

--- test/main.go
+++ test/main.go
@@ -1,12 +1,12 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"piscine"
-)
+// 	"piscine"
+// )
 
-func main() {
-	fmt.Println(piscine.AppendRange(5, 10))
-	fmt.Println(piscine.AppendRange(10, 5))
-}
+// func main() {
+// 	fmt.Println(piscine.AppendRange(5, 10))
+// 	fmt.Println(piscine.AppendRange(10, 5))
+// }
