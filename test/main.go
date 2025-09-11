5cbcb00cc6ebc0e97cca8c964133ae050196cf9d
tvairakt <t.vairaktaris@gmail.com>
2025-09-11 18:31:47 +0300

    create the makerange function

--- test/main.go
+++ test/main.go
@@ -7,6 +7,6 @@ package main
 // )
 
 // func main() {
-// 	fmt.Println(piscine.AppendRange(5, 10))
-// 	fmt.Println(piscine.AppendRange(10, 5))
+// 	fmt.Println(piscine.MakeRange(5, 10))
+// 	fmt.Println(piscine.MakeRange(10, 5))
 // }
