1a8d637322ee28b52da95082c39f26b90ac2899c
tvairakt <t.vairaktaris@gmail.com>
2025-09-11 14:25:37 +0300

    fix: remove built in int64

--- printnbrbase.go
+++ printnbrbase.go
@@ -2,24 +2,6 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-// helper: validate base
-func isValidBase(base string) bool {
-	if len(base) < 2 {
-		return false
-	}
-	seen := make(map[rune]bool)
-	for _, r := range base {
-		if r == '+' || r == '-' {
-			return false
-		}
-		if seen[r] {
-			return false
-		}
-		seen[r] = true
-	}
-	return true
-}
-
 func PrintNbrBase(nbr int, base string) {
 	if !isValidBase(base) {
 		z01.PrintRune('N')
@@ -28,22 +10,52 @@ func PrintNbrBase(nbr int, base string) {
 	}
 
 	baseRunes := []rune(base)
-	baseLen := int64(len(baseRunes))
-	n := int64(nbr)
+	b := len(baseRunes)
 
-	if n < 0 {
+	if nbr == 0 {
+		z01.PrintRune(baseRunes[0])
+		return
+	}
+
+	if nbr < 0 {
 		z01.PrintRune('-')
+	}
+
+	n := nbr
+	if n > 0 {
 		n = -n
 	}
 
-	// recursive printing
-	var printRec func(int64)
-	printRec = func(num int64) {
-		if num >= baseLen {
-			printRec(num / baseLen)
-		}
-		z01.PrintRune(baseRunes[num%baseLen])
+	printNegInBase(n, b, baseRunes)
+}
+
+func printNegInBase(n int, b int, baseRunes []rune) {
+	q := n / b
+
+	r := -(n % b)
+
+	if q != 0 {
+		printNegInBase(q, b, baseRunes)
+	}
+	z01.PrintRune(baseRunes[r])
+}
+
+func isValidBase(base string) bool {
+	br := []rune(base)
+
+	if len(br) < 2 {
+		return false
 	}
 
-	printRec(n)
+	for i := 0; i < len(br); i++ {
+		if br[i] == '+' || br[i] == '-' {
+			return false
+		}
+		for j := i + 1; j < len(br); j++ {
+			if br[i] == br[j] {
+				return false
+			}
+		}
+	}
+	return true
 }
--- test/main.go
+++ test/main.go
@@ -1,20 +1,20 @@
 package main
 
-// import (
-// 	"piscine"
+import (
+	"piscine"
 
-// 	"github.com/01-edu/z01"
-// )
+	"github.com/01-edu/z01"
+)
 
-// func main() {
-// 	piscine.PrintNbrBase(125, "0123456789")
-// 	z01.PrintRune('\n')
-// 	piscine.PrintNbrBase(-125, "01")
-// 	z01.PrintRune('\n')
-// 	piscine.PrintNbrBase(125, "0123456789ABCDEF")
-// 	z01.PrintRune('\n')
-// 	piscine.PrintNbrBase(-125, "choumi")
-// 	z01.PrintRune('\n')
-// 	piscine.PrintNbrBase(125, "aa")
-// 	z01.PrintRune('\n')
-// }
+func main() {
+	piscine.PrintNbrBase(125, "0123456789")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "01")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(125, "0123456789ABCDEF")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "choumi")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(125, "aa")
+	z01.PrintRune('\n')
+}
