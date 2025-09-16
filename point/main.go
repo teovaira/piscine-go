f80c732f73a99380df88e7fc1f9d010239403927
tvairakt <t.vairaktaris@gmail.com>
2025-09-16 10:25:05 +0300

    create the point program

--- point/main.go
+++ point/main.go
@@ -12,11 +12,42 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func printString(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func printInt(nbr int) {
+	if nbr == 0 {
+		z01.PrintRune('0')
+	}
+
+	if nbr < 0 {
+		z01.PrintRune('-')
+		nbr = -nbr
+	}
+
+	var digits []rune
+	for nbr > 0 {
+		d := nbr % 10
+		digits = append([]rune{rune(d +'0')}, digits...)
+		nbr/=10
+	}
+	for _,r := range digits {
+		z01.PrintRune(r)
+	}
+
+}
+
 func main() {
 	points := point{}
 
 	setPoint(&points)
+	printString("x = ")
+	printInt(points.x)
+	printString(", y = ")
+	printInt(points.y)
 
-	z01.PrintRune(rune(points.x - '0'))
-	z01.PrintRune(rune(points.y - '0'))
+	
 }
