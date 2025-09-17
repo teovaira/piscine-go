c14e8ee997937b25ab8ca6db478ae304a9a8d6a0
tvairakt <t.vairaktaris@gmail.com>
2025-09-17 10:36:18 +0300

    fix: delete the testing code because of the checker

--- test/main.go
+++ test/main.go
@@ -1,18 +1,2 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"piscine"
-// )
-
-// func f(a, b int) int { return a - b } 
-
-// func main() {
-// 	a1 := []int{0, 1, 2, 3, 4, 5}
-// 	a2 := []int{0, 2, 1, 3}
-// 	a3 := []int{5, 4, 4, 1} 
-
-// 	fmt.Println(piscine.IsSorted(f, a1)) 
-// 	fmt.Println(piscine.IsSorted(f, a2)) 
-// 	fmt.Println(piscine.IsSorted(f, a3)) 
-// }
