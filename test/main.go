67798c485df314922860231a3d55e2ef980d527c
tvairakt <t.vairaktaris@gmail.com>
2025-09-17 10:33:36 +0300

    fix: comment out the testing code

--- test/main.go
+++ test/main.go
@@ -1,19 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
+// import (
+// 	"fmt"
+// 	"piscine"
+// )
 
-func f(a, b int) int { return a - b } 
+// func f(a, b int) int { return a - b } 
 
-func main() {
-	a1 := []int{0, 1, 2, 3, 4, 5}
-	a2 := []int{0, 2, 1, 3}
-	a3 := []int{5, 4, 4, 1} 
+// func main() {
+// 	a1 := []int{0, 1, 2, 3, 4, 5}
+// 	a2 := []int{0, 2, 1, 3}
+// 	a3 := []int{5, 4, 4, 1} 
 
-	fmt.Println(piscine.IsSorted(f, a1)) 
-	fmt.Println(piscine.IsSorted(f, a2)) 
-	fmt.Println(piscine.IsSorted(f, a3)) 
-}
+// 	fmt.Println(piscine.IsSorted(f, a1)) 
+// 	fmt.Println(piscine.IsSorted(f, a2)) 
+// 	fmt.Println(piscine.IsSorted(f, a3)) 
+// }
 
