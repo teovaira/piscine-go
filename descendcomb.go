ebc9292c878fcf7fc8e578960248920cdca4a881
tvairakt <t.vairaktaris@gmail.com>
2025-09-18 11:39:15 +0300

    fix: remove the trailing comma

--- descendcomb.go
+++ descendcomb.go
@@ -1,6 +1,10 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
 
 func DescendComb() {
 	for r := 99; r >= 0; r-- {
@@ -12,11 +16,13 @@ func DescendComb() {
 			z01.PrintRune(rune(j/10 + '0'))
 			z01.PrintRune(rune(j%10 + '0'))
 
-			if !(r == 0o0 && j == 1) {
+			if !(r == 1 && j == 0) {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 
 		}
 	}
+	z01.PrintRune('\n')
+	os.Exit(1)
 }
