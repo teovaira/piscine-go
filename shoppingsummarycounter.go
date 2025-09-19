6ae6630c3f9511627698de144346ad15e0dcfa87
tvairakt <t.vairaktaris@gmail.com>
2025-09-19 14:45:56 +0300

    fix: format output

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -2,24 +2,23 @@ package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
 	counts := make(map[string]int)
-	i := 0
-	n := len(str)
+	current := ""
+	inWord := false
 
-	for i < n {
-		for i < n && str[i] == ' ' {
-			i++
+	for _, r := range str {
+		if r == ' ' {
+			if inWord {
+				counts[current]++
+				current = ""
+				inWord = false
+			}
+			continue
 		}
-		if i >= n {
-			break
-		}
-		j := i
-		for j < n && str[j] != ' ' {
-			j++
-		}
-		word := str[i:j]
-		counts[word]++
-		i = j
+		current += string(r)
+		inWord = true
 	}
-
+	if inWord {
+		counts[current]++
+	}
 	return counts
 }
