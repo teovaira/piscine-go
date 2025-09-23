63c26711c7cffee5a618b46e15efd1a65efc38ab
tvairakt <t.vairaktaris@gmail.com>
2025-09-23 10:12:28 +0300

    fix: comment out the test code

--- test/main.go
+++ test/main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"piscine"
-)
+// 	"piscine"
+// )
 
-func main() {
-	link := &piscine.List{}
+// func main() {
+// 	link := &piscine.List{}
 
-	piscine.ListPushFront(link, "Hello")
-	piscine.ListPushFront(link, "2")
-	piscine.ListPushFront(link, "you")
-	piscine.ListPushFront(link, "man")
+// 	piscine.ListPushFront(link, "Hello")
+// 	piscine.ListPushFront(link, "2")
+// 	piscine.ListPushFront(link, "you")
+// 	piscine.ListPushFront(link, "man")
 
-	fmt.Println(piscine.ListSize(link))
-}
+// 	fmt.Println(piscine.ListSize(link))
+// }
