c66821c7c481731eaca79aad8b1254b25b6330dd
tvairakt <t.vairaktaris@gmail.com>
2025-09-23 19:50:10 +0300

    create the BTreeInsertData

--- test/main.go
+++ test/main.go
@@ -1,48 +1,18 @@
 package main
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"piscine"
-// )
+	"piscine"
+)
 
-// func PrintElem(node *piscine.NodeL) {
-// 	fmt.Println(node.Data)
-// }
-
-// func StringToInt(node *piscine.NodeL) {
-// 	node.Data = 2
-// }
-
-// func PrintList(l *piscine.List) {
-// 	it := l.Head
-// 	for it != nil {
-// 		fmt.Print(it.Data, "->")
-// 		it = it.Next
-// 	}
-// 	fmt.Print("nil", "\n")
-// }
-
-// func main() {
-// 	link := &piscine.List{}
-
-// 	piscine.ListPushBack(link, 1)
-// 	piscine.ListPushBack(link, "hello")
-// 	piscine.ListPushBack(link, 3)
-// 	piscine.ListPushBack(link, "there")
-// 	piscine.ListPushBack(link, 23)
-// 	piscine.ListPushBack(link, "!")
-// 	piscine.ListPushBack(link, 54)
-
-// 	PrintList(link)
-
-// 	fmt.Println("--------function applied--------")
-// 	piscine.ListForEachIf(link, PrintElem, piscine.IsPositiveNode)
-
-// 	piscine.ListForEachIf(link, StringToInt, piscine.IsAlNode)
-
-// 	fmt.Println("--------function applied--------")
-// 	PrintList(link)
-
-// 	fmt.Println()
-// }
+func main() {
+	root := &piscine.TreeNode{Data: "4"}
+	piscine.BTreeInsertData(root, "1")
+	piscine.BTreeInsertData(root, "7")
+	piscine.BTreeInsertData(root, "5")
+	fmt.Println(root.Left.Data)
+	fmt.Println(root.Data)
+	fmt.Println(root.Right.Left.Data)
+	fmt.Println(root.Right.Data)
+}
