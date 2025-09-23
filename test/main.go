bd9eb18a007dd0a73463d8a52995454f2185addb
tvairakt <t.vairaktaris@gmail.com>
2025-09-23 09:53:24 +0300

    create the ListPushFront function

--- test/main.go
+++ test/main.go
@@ -7,8 +7,17 @@ package main
 // )
 
 // func main() {
-// 	summary := "Burger Water Carrot Coffee Water Water Chips Carrot Carrot Burger Carrot Water"
-// 	for index, element := range piscine.ShoppingSummaryCounter(summary) {
-// 		fmt.Println(index, "=>", element)
+
+// 	link := &piscine.List{}
+
+// 	piscine.ListPushFront(link, "Hello")
+// 	piscine.ListPushFront(link, "man")
+// 	piscine.ListPushFront(link, "how are you")
+
+// 	it := link.Head
+// 	for it != nil {
+// 		fmt.Print(it.Data, " ")
+// 		it = it.Next
 // 	}
+// 	fmt.Println()
 // }
